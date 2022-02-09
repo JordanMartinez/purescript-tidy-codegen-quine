707e1353686ef748dec361579ab0bb20d9bc40e1
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-02-09 12:56:36 -0600

    Generate output in same folder

--- test/snapshots/Example1/Generate.purs
+++ test/snapshots/Example1/Generate.purs
@@ -11,6 +11,6 @@ import Tidy.Codegen (printModule)
 import Tidy.Codegen.Monad (codegenModule)
 
 main :: Effect Unit
-main = launchAff_ do writeTextFile UTF8 "ignore/Example1.purs" generatedMod
+main = launchAff_ do writeTextFile UTF8 "test/snapshots/Example1/Example1.purs" generatedMod
   where
   generatedMod = printModule $ unsafePartial $ codegenModule "Test.Snapshots.Example1" do pure unit
