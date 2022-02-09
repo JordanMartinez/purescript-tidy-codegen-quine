a053c92c2204df6666027c37014cd1beca2603fe
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-02-09 12:53:24 -0600

    Generate initial broken example

---
+++ test/snapshots/Example1/Generate.purs
@@ -0,0 +1,16 @@
+module Test.Snapshots.Example1.Generate where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff (writeTextFile)
+import Partial.Unsafe (unsafePartial)
+import Tidy.Codegen (printModule)
+import Tidy.Codegen.Monad (codegenModule)
+
+main :: Effect Unit
+main = launchAff_ do writeTextFile UTF8 "ignore/Example1.purs" generatedMod
+  where
+  generatedMod = printModule $ unsafePartial $ codegenModule "Test.Snapshots.Example1" do pure unit
